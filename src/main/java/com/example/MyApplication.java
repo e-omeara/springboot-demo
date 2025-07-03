41fd382c5d642f45d535adfbeff0d1061c178e79
Elizabeth O'Meara <eomeara@Elizabeths-MacBook-Pro-2.local>
2025-07-03 09:31:49 -0400

    added html pages

--- src/main/java/com/example/MyApplication.java
+++ src/main/java/com/example/MyApplication.java
@@ -1,17 +1,19 @@
 package com.example;
 
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController //indicates that this is a web controller
+
+@Controller
 @SpringBootApplication  //meta annocation that combines @SpringBootConfiguration, @EnableAutoConfiguration, and @ComponentScan
 public class MyApplication {
 
-	@RequestMapping("/")    //this provides routing information - in this case for the root URL
-	String home() {
-		return "Hello World!";
+	@RequestMapping("/meow")    //this provides routing information - in this case for the root URL
+	public String home() {
+		return "meow.html"; //this will return the meow.html file from the resources/static directory
 	}
 
 	public static void main(String[] args) {
